26304e7f102e0bcc3c06b5ee694f1a7376779003
Rizki <muhammadrizkitkj123@gmail.com>
2026-02-28 09:38:05 +0800

    fix:firebase

--- app/_services/userService.ts
+++ app/_services/userService.ts
@@ -1,13 +1,13 @@
 import { cookies } from "next/headers";
-import { adminAuth, adminDb } from "../_lib/firebaseAdmin";
+import { getAdminAuth, getAdminDb } from "../_lib/firebaseAdmin";
 
 export const getUser = async () => {
     const token = (await cookies()).get("session")?.value;
     if (!token) return null;
 
-    const decodedToken = await adminAuth.verifySessionCookie(token);
+    const decodedToken = await getAdminAuth().verifySessionCookie(token);
     if (!decodedToken) return null;
-    const userRef = adminDb.ref("users/" + decodedToken.uid);
+    const userRef = getAdminDb().ref("users/" + decodedToken.uid);
     const snapshot = await userRef.get();
     if (!snapshot.exists()) return null;
 
