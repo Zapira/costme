acd2854daf503ece68b5bbe439321fa15dcc73fc
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-05 16:08:50 +0800

    feat:update

--- app/(routes)/app/home/page.tsx
+++ app/(routes)/app/home/page.tsx
@@ -5,8 +5,15 @@ import BottomBar from "@/app/_components/shared/bottomBar";
 import Navbar from "@/app/_components/shared/navbar";
 import { getUser } from "@/app/_services/userService";
 
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default async function Home() {
+    await sleep(2000); 
+
     const user = await getUser();
+
     return (
         <div className="bg-gray-50 p-6 shadow-md max-w-lg mx-auto min-h-screen ">
             <Navbar user={user} />
