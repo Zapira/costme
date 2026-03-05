385c32bbd20fba6ee8292b6e42c7b2c6f99c38bb
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-05 16:09:07 +0800

    feat:update

--- app/(routes)/app/management/page.tsx
+++ app/(routes)/app/management/page.tsx
@@ -1,13 +1,12 @@
-import AccountCard from "@/app/_components/home/accountCard";
-import LastTransaction from "@/app/_components/home/lastTransaction";
-import RemainingCard from "@/app/_components/home/remainingCard";
 import Content from "@/app/_components/management/content";
 import BottomBar from "@/app/_components/shared/bottomBar";
-import Navbar from "@/app/_components/shared/navbar";
-import { getUser } from "@/app/_services/userService";
+
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 export default async function Management() {
-    const user = await getUser();
+    await sleep(2000);
     return (
         <div className="bg-gray-50 p-6 shadow-md max-w-lg mx-auto min-h-screen ">
             <Content />
