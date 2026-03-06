deebadcbe9539d8eb59f132ce04c04851887f76b
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-06 11:22:39 +0800

    fix:session login

--- proxy.ts
+++ proxy.ts
@@ -1,35 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(req: NextRequest) {
+    const token = req.cookies.get("session")?.value;
     const { pathname } = req.nextUrl;
 
-    // ambil cookie session
-    const token = req.cookies.get("session")?.value;
-
-    // validasi token
-    const isAuth =
-        token &&
-        token !== "undefined" &&
-        token !== "null" &&
-        token.trim() !== "";
-
-    // jika belum login tapi akses halaman app
-    if (!isAuth && pathname.startsWith("/app")) {
-        return NextResponse.redirect(new URL("/", req.url));
+    if (!token) {
+        if (pathname.startsWith("/app")) {
+            const res = NextResponse.redirect(new URL("/", req.url));
+            res.cookies.delete("session");
+            return res;
+        }
     }
 
-    // jika sudah login tapi masih di halaman login
-    if (isAuth && pathname === "/") {
+    if (token && pathname === "/") {
         return NextResponse.redirect(new URL("/app/home", req.url));
     }
 
     return NextResponse.next();
 }
 
-// route yang diproteksi proxy
 export const config = {
-    matcher: [
-        "/",
-        "/app/:path*"
-    ],
+    matcher: ["/", "/app/:path*"],
 };
