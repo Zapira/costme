aa68a8165d0e4a92c9f8a9b5239239c117225d1c
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-09 12:15:05 +0800

    fix:proxy

--- proxy.ts
+++ proxy.ts
@@ -6,13 +6,11 @@ export function proxy(req: NextRequest) {
 
     const isAuth = !!session;
 
-    // jika belum login dan akses /app
     if (!isAuth && pathname.startsWith("/app")) {
         return NextResponse.redirect(new URL("/auth/login", req.url));
     }
 
-    // jika sudah login dan buka login page
-    if (isAuth && pathname === "/auth/login") {
+    if (isAuth && pathname.startsWith("/auth/login")) {
         return NextResponse.redirect(new URL("/app/home", req.url));
     }
 
@@ -20,5 +18,5 @@ export function proxy(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/app/:path*", "/login"],
+    matcher: ["/app/:path*", "/auth/:path*"],
 };
