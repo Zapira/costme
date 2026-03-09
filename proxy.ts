193ca53de7108c93a8be6580f8cc11493a8b0801
Rizki <muhammadrizkitkj123@gmail.com>
2026-03-09 12:02:23 +0800

    fix:proxy

--- proxy.ts
+++ proxy.ts
@@ -1,30 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function proxy(req: NextRequest) {
+export function middleware(req: NextRequest) {
     const session = req.cookies.get("session")?.value;
     const { pathname } = req.nextUrl;
 
-    let isAuth = false;
+    const isAuth = !!session;
 
-    if (session) {
-        try {
-            const payload = JSON.parse(
-                Buffer.from(session.split(".")[1], "base64").toString()
-            );
-
-            if (payload.aud === "costme-c141c") {
-                isAuth = true;
-            }
-        } catch (e) {
-            isAuth = false;
-        }
-    }
-
+    // jika belum login dan akses /app
     if (!isAuth && pathname.startsWith("/app")) {
-        return NextResponse.redirect(new URL("/", req.url));
+        return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    if (isAuth && pathname === "/") {
+    // jika sudah login dan buka login page
+    if (isAuth && pathname === "/login") {
         return NextResponse.redirect(new URL("/app/home", req.url));
     }
 
@@ -32,5 +20,5 @@ export function proxy(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/", "/app/:path*"],
+    matcher: ["/app/:path*", "/login"],
 };
